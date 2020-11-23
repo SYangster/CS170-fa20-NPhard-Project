f777c1fd37148d0fd45340186a5c477e165e69f2
Shivam Singhal <shivamsinghal@berkeley.edu>
2020-11-22 17:41:11 -0800

    random inputs generated

---
+++ random_inputs.py
@@ -0,0 +1,19 @@
+import random
+import json
+import _pickle as pickle
+
+
+num_students = 50
+smax = round(random.uniform(50, 100), 3)
+dict = {}
+for i in range(1, num_students+1):
+	for j in range(1, num_students+1):
+		if (i, j) not in dict and i != j and (j,i) not in dict:
+			dict[(i,j)] = (round(random.uniform(0, 36), 3), round(random.uniform(3, 45), 3))
+
+
+with open('50.in', 'w') as f:
+    print(num_students, file=f)
+    print(smax, file=f)
+    for x in dict:
+    	print(x[0], x[1], dict[x][0], dict[x][1], file=f)
