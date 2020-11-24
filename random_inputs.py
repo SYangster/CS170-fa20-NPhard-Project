dfb8d2cb6eda2efdc45826beebe5b8a06d2a4de5
Shivam Singhal <shivamsinghal@berkeley.edu>
2020-11-23 17:08:20 -0800

    changed 10

--- random_inputs.py
+++ random_inputs.py
@@ -4,14 +4,14 @@ import _pickle as pickle
 
 
 num_students = 50
-smax = round(random.uniform(90, 100), 3)
+smax = round(random.uniform(68, 100), 3)
 dict = {}
 
 def write_file(num_students):
 	for i in range(num_students):
 		for j in range(num_students):
 			if (i, j) not in dict and i != j and (j,i) not in dict:
-				dict[(i,j)] = (round(random.uniform(0, 36), 3), round(random.uniform(1, 5), 3))
+				dict[(i,j)] = (round(random.uniform(0, 36), 3), round(random.uniform(1, 26), 3))
 
 
 	file = str(num_students) + '.in'
@@ -22,5 +22,3 @@ def write_file(num_students):
 	    	print(x[0], x[1], dict[x][0], dict[x][1], file=f)
 
 write_file(10)
-write_file(20)
-write_file(50)
