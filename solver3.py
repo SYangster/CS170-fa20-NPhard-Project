d2ebaefbcca8e045d3b30e92758035718e890e88
Sean Yang <seanyang@berkeley.edu>
2020-11-24 17:51:33 -0800

    got 1113 on 50.in

--- solver3.py
+++ solver3.py
@@ -86,6 +86,8 @@ def generate_start_state(G, s):
         students = []
         for student in (G.adjacency()):
             students.append(student[0])
+        #k = np.random.randint(1, len(students)/2+3) #HYPERPARAMETER: (maybe do binary search) use to pick range of random room numbers
+
         k = np.random.randint(1, len(students)/2+3) #HYPERPARAMETER: (maybe do binary search) use to pick range of random room numbers
 
         invalid_sol = False
@@ -310,7 +312,7 @@ if __name__ == '__main__':
     max_k = 0
     for student in (G.adjacency()):
         max_k += 1 
-    for i in range(0, 2): #HYPERPARAMETER: how many iterations of whole algorithm to do
+    for i in range(0, 1): #HYPERPARAMETER: how many iterations of whole algorithm to do
         print("Iteration:" + str(i))
         D, k = solve(G, s)
         assert is_valid_solution(D, G, s, k)
